46b916ebad375477aa996384a78fa40da445da0d
Mary Saray <msaray.ms@outlook.com>
2021-10-20 15:31:23 -0700

    Created IdentityHelper for roles

---
+++ LeavingALegacy/Models/IdentityHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeavingALegacy.Models
+{
+    public static class IdentityHelper
+    {
+        public const string Admin = "Admin";
+        public const string Member = "Member";
+
+        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            RoleManager<IdentityRole> roleManager = provider.GetService<RoleManager<IdentityRole>>();
+
+            foreach (string role in roles)
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
