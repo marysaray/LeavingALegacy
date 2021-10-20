566ebddaa7173a17952427a335d1833bb2235916
Mary Saray <msaray.ms@outlook.com>
2021-10-20 10:57:18 -0700

    Added Username property to Login

--- LeavingALegacy/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ LeavingALegacy/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -12,6 +12,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+#nullable disable
+
 namespace LeavingALegacy.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
@@ -43,8 +45,7 @@ namespace LeavingALegacy.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string Username { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -81,7 +82,7 @@ namespace LeavingALegacy.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
