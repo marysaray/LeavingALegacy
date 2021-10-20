a1ae53506e610e8ad78a2fb434e0f5bc65724522
Mary Saray <82470838+marysaray@users.noreply.github.com>
2021-10-20 15:36:21 -0700

    Added Identity Roles (#22)
    * Disabled required confirmed account for email.
    
    * Renamed database
    
    * Scaffolded ASP.NET Identity into project
    
    * Added Username property to Login
    
    * Commented out remember me checkbox feature
    
    * Added Username label to register view page
    
    * Added customized Username as a Login feature
    
    * Added IdentityOptions for changeable web-data settings
    
    * Added IdentityRole support to DefaultIdentity
    
    * Created IdentityHelper for roles
    
    * Added serviceProvider for roles

--- LeavingALegacy/Startup.cs
+++ LeavingALegacy/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LeavingALegacy.Models;
 
 namespace LeavingALegacy
 {
@@ -32,13 +33,36 @@ namespace LeavingALegacy
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            // Version 5.0 https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-5.0&tabs=netcore-cli
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async Task ConfigureAsync(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -66,6 +90,11 @@ namespace LeavingALegacy
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            var serviceProvider = app.ApplicationServices.GetRequiredService<IServiceProvider>().CreateScope();
+            // Create default roles
+            await IdentityHelper.CreateRoles(serviceProvider.ServiceProvider, IdentityHelper.Admin, IdentityHelper.Member);
+            // Create default admin
         }
     }
 }
