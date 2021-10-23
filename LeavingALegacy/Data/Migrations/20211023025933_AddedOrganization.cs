3192489d453fea3a952d2359e0a61097b24b795f
Mary Saray <82470838+marysaray@users.noreply.github.com>
2021-10-22 20:23:21 -0700

    Issue#17 create organization class (#31)
    * Created organization class
    
    * Added organization to Db context and database
    
    * Created organization controller and scaffolded view page

---
+++ LeavingALegacy/Data/Migrations/20211023025933_AddedOrganization.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LeavingALegacy.Data.Migrations
+{
+    public partial class AddedOrganization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Development = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ManagerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Organizations_Administrators_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "Administrators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Organizations_ManagerId",
+                table: "Organizations",
+                column: "ManagerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
---
+++ LeavingALegacy/Models/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeavingALegacy.Models
+{
+    public class Organization
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Development { get; set; }
+
+        public string Description { get; set; }
+
+        public Administrator Manager { get; set; }
+    }
+}
