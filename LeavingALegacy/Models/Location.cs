5092482491478d0293f6ea8af2a8417f20103b23
Mary Saray <msaray.ms@outlook.com>
2021-10-23 15:49:34 -0700

    Added location class

---
+++ LeavingALegacy/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeavingALegacy.Models
+{
+    public class Location
+    {
+        [Key]
+        public int SecId { get; set; }
+
+        public string LocationName { get; set; }
+    }
+}
