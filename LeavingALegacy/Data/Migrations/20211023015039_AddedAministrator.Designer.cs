94f819612b9fac14fc52d9f093115bbcd689d262
Mary Saray <msaray.ms@outlook.com>
2021-10-22 19:44:23 -0700

    Added administrators to Db context

--- LeavingALegacy/Data/Migrations/20211023015039_AddedAministrator.Designer.cs
+++ LeavingALegacy/Data/Migrations/20211023015039_AddedAministrator.Designer.cs
@@ -4,14 +4,16 @@ using LeavingALegacy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LeavingALegacy.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211023015039_AddedAministrator")]
+    partial class AddedAministrator
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LeavingALegacy/Data/Migrations/20211023015039_AddedAministrator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LeavingALegacy.Data.Migrations
+{
+    public partial class AddedAministrator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Administrators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Administrators", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Administrators");
+        }
+    }
+}
