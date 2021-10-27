a0faa00d7d30fefb04ef81540de246d4927f8462
Mary Saray <msaray.ms@outlook.com>
2021-10-27 10:17:19 -0700

    Created OrganizationCreateViewModel with view properties

--- LeavingALegacy/Models/Organization.cs
+++ LeavingALegacy/Models/Organization.cs
@@ -19,4 +19,15 @@ namespace LeavingALegacy.Models
 
         public Location Place { get; set; }
     }
+
+    public class OrganizationCreateViewModel
+    { 
+        public string Development { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Administrator> Managers { get; set; }
+
+        public List<Location> Places { get; set; }
+    }
 }
